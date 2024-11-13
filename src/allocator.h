78d8a7805c52b2e0f3ddb4e7f4f63e43e2a478d6
Yifan LI <liyifan0117@gmail.com>
2024-11-13 14:35:44 +0100

    finish the part of lock

--- src/allocator.h
+++
@@ -1,9 +0,0 @@
-#ifndef ALLOCATOR_H
-#define ALLOCATOR_H
-
-#include <stddef.h> // 用于 size_t 类型
-
-void* my_malloc(size_t size);
-void my_free(void* ptr);
-
-#endif // ALLOCATOR_H
--- src/myAllocator.c
+++ src/myAllocator.c
@@ -4,6 +4,8 @@
 #include <bits/mman-linux.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
+
 
 // 定义一个结构体来存储块的大小信息
 typedef struct block_header {
@@ -17,7 +19,12 @@ typedef struct free_block {
 
 free_block_t* free_list = NULL;
 
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;//增加全局锁
+
+
 void* my_malloc(size_t size) {
+    pthread_mutex_lock(&lock); // 加锁
+
     size_t total_size = sizeof(size_t) + size; // 包括块头的总大小
     free_block_t* best_fit = NULL;
     free_block_t** best_fit_prev = NULL;
@@ -37,6 +44,7 @@ void* my_malloc(size_t size) {
     // 如果找到了合适的块
     if (best_fit != NULL) {
         *best_fit_prev = best_fit->next; // 从链表中移除最佳块
+        pthread_mutex_unlock(&lock); // 解锁
         return (void*)((char*)best_fit + sizeof(size_t));
     }
 
@@ -47,16 +55,19 @@ void* my_malloc(size_t size) {
     void* ptr = mmap(NULL, alloc_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (ptr == MAP_FAILED) {
         perror("mmap failed");
+        pthread_mutex_unlock(&lock); // 解锁
         return NULL;
     }
 
     *((size_t*)ptr) = alloc_size;
+    pthread_mutex_unlock(&lock); // 解锁
     return (void*)((char*)ptr + sizeof(size_t));
 }
 
 void my_free(void* ptr) {
     if (ptr == NULL) return;
-
+    pthread_mutex_lock(&lock); // 加锁
+    
     // 获取块头信息
     size_t* block_start = (size_t*)((char*)ptr - sizeof(size_t));
     size_t alloc_size = *block_start;
@@ -66,4 +77,6 @@ void my_free(void* ptr) {
     new_block->size = alloc_size;
     new_block->next = free_list;
     free_list = new_block;
+
+      pthread_mutex_unlock(&lock); // 解锁
 }

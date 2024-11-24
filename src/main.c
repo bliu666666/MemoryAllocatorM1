b8028cfb7b47449fd27f8de3e3ed02cb3d47c301
Bowen LIU <bowen.liu@ens.uvsq.fr>
2024-11-24 23:09:46 +0100

    perf:different memory sizes are allocated each time to better test stability in the performance test loop.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include "perf_cmp.c"
 
 int main(int argc,char **argv) {
-    if (argc!=3)
+    if (argc!=5)
     {
-        fprintf(stderr,"Usage: need 3 arguments\n");
+        fprintf(stderr,"Usage: need 5 arguments\n");
         exit(0);
     }
     size_t size=atoi(argv[1]);// Allocate argv[1]bytes of memory
     int num_allocations=atoi(argv[2]);
+    size_t min_allocation_size = atoi(argv[3]); // Minimum allocation size
+    size_t max_allocation_size = atoi(argv[4]); // Maximum allocation size
 
     printf("Allocating %zu bytes of memory...\n", size);
     void* ptr = my_malloc(size);
@@ -35,10 +37,10 @@ int main(int argc,char **argv) {
 
     // Test the performance of different allocators
     printf("Testing custom allocator (my_malloc/my_free)...\n");
-    test_my_allocator_performance(num_allocations,size);
+    test_my_allocator_performance(num_allocations,min_allocation_size,max_allocation_size);
 
     printf("Testing system allocator (malloc/free)...\n");
-    test_system_allocator_performance(num_allocations,size);
+    test_system_allocator_performance(num_allocations,min_allocation_size,max_allocation_size);
 
     return 0;
 }
--- src/perf_cmp.c
+++ src/perf_cmp.c
@@ -8,14 +8,22 @@ double calculate_time(struct timespec start, struct timespec end) {
     return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
 }
 
+// Generate random allocation size between min_size and max_size
+size_t generate_random_size(size_t min_size, size_t max_size) {
+    return min_size + rand() % (max_size - min_size + 1);
+}
+
 // Test the performance of my_malloc/my_free
-void test_my_allocator_performance(int num_allocations,size_t allocation_size) {
+void test_my_allocator_performance(int num_allocations,size_t min_allocation_size, size_t max_allocation_size) {
     struct timespec start, end;
+
+    srand(time(NULL));
 
     // Start timing
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     for (int i = 0; i < num_allocations; i++) {
+        size_t allocation_size = generate_random_size(min_allocation_size, max_allocation_size);
         void* ptr = my_malloc(allocation_size);
         if (ptr == NULL) {
             fprintf(stderr, "my_malloc failed on iteration %d\n", i);
@@ -28,17 +36,18 @@ void test_my_allocator_performance(int num_allocations,size_t allocation_size) {
     clock_gettime(CLOCK_MONOTONIC, &end);
     double time_spent = calculate_time(start, end);
 
-    printf("my_malloc/my_free: %d allocations of %zu bytes took %f seconds\n", num_allocations, allocation_size, time_spent);
+    printf("my_malloc/my_free: %d allocations of sizes between %zu and %zu bytes took %f seconds\n",num_allocations, min_allocation_size, max_allocation_size, time_spent);
 }
 
 // Test the performance of malloc/free
-void test_system_allocator_performance(int num_allocations,size_t allocation_size) {
+void test_system_allocator_performance(int num_allocations,size_t min_allocation_size, size_t max_allocation_size) {
     struct timespec start, end;
 
     // Start timing
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     for (int i = 0; i < num_allocations; i++) {
+        size_t allocation_size = generate_random_size(min_allocation_size, max_allocation_size);
         void* ptr = malloc(allocation_size);
         if (ptr == NULL) {
             fprintf(stderr, "malloc failed on iteration %d\n", i);
@@ -51,5 +60,6 @@ void test_system_allocator_performance(int num_allocations,size_t allocation_siz
     clock_gettime(CLOCK_MONOTONIC, &end);
     double time_spent = calculate_time(start, end);
 
-    printf("malloc/free: %d allocations of %zu bytes took %f seconds\n", num_allocations, allocation_size, time_spent);
+    printf("malloc/free: %d allocations of sizes between %zu and %zu bytes took %f seconds\n",
+           num_allocations, min_allocation_size, max_allocation_size, time_spent);
 }

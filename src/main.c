cd289996c54d882572a4330263c93f3866ffce9c
Bowen LIU <bowen.liu@ens.uvsq.fr>
2024-12-03 11:35:05 +0100

    feat:add multi-threaded performance testing capabilities

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
 #include "perf_cmp.c"
 
 int main(int argc,char **argv) {
-    if (argc!=5)
+    if (argc!=6)
     {
-        fprintf(stderr,"Usage: need 5 arguments\n");
+        fprintf(stderr,"Usage: need 6 arguments\n");
         exit(0);
     }
     size_t size=atoi(argv[1]);// Allocate argv[1]bytes of memory
     int num_allocations=atoi(argv[2]);
     size_t min_allocation_size = atoi(argv[3]); // Minimum allocation size
     size_t max_allocation_size = atoi(argv[4]); // Maximum allocation size
-
+    int num_threads=atoi(argv[5]); // Number of threads
     printf("Allocating %zu bytes of memory...\n", size);
     void* ptr = my_malloc(size);
 
@@ -41,6 +41,12 @@ int main(int argc,char **argv) {
 
     printf("Testing system allocator (malloc/free)...\n");
     test_system_allocator_performance(num_allocations,min_allocation_size,max_allocation_size);
+
+    printf("Testing custom allocator (multi-threaded, %d threads)...\n",num_threads);
+    test_multithread_allocator_performance(num_allocations, num_threads, min_allocation_size, max_allocation_size);
+
+    printf("Testing system allocator (multi-threaded, %d threads)...\n",num_threads);
+    test_multithread_system_allocator_performance(num_allocations, num_threads, min_allocation_size, max_allocation_size);
 
     return 0;
 }
--- src/perf_cmp.c
+++ src/perf_cmp.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include "myAllocator.c"
+#include <pthread.h>
+
+typedef struct{
+    int num_allocations;
+    size_t min_allocation_size;
+    size_t max_allocation_size;
+}thread_data_t;//Task data for each thread
 
 // Calculate time difference
 double calculate_time(struct timespec start, struct timespec end) {
@@ -11,6 +18,42 @@ double calculate_time(struct timespec start, struct timespec end) {
 // Generate random allocation size between min_size and max_size
 size_t generate_random_size(size_t min_size, size_t max_size) {
     return min_size + rand() % (max_size - min_size + 1);
+}
+
+//Tasks executed by a single thread of my_malloc/my_free
+void *thread_task_custom(void *arg)
+{
+    thread_data_t data=*(thread_data_t*)arg;
+    srand(time(NULL));
+    for (int i=0;i<data.num_allocations;i++)
+    {
+        size_t allocation_size= generate_random_size(data.min_allocation_size,data.max_allocation_size);
+        void* ptr = my_malloc(allocation_size);
+        if (ptr == NULL) {
+            fprintf(stderr, "my_malloc failed in thread %lu at iteration %d\n", pthread_self(),i);
+            pthread_exit(NULL);
+        }
+        my_free(ptr);
+    }
+    pthread_exit(NULL);
+}
+
+//Tasks executed by a single thread of malloc/free
+void *thread_task_system(void *arg)
+{
+    thread_data_t data=*(thread_data_t*)arg;
+    srand(time(NULL));
+    for (int i=0;i<data.num_allocations;i++)
+    {
+        size_t allocation_size= generate_random_size(data.min_allocation_size,data.max_allocation_size);
+        void* ptr = malloc(allocation_size);
+        if (ptr == NULL) {
+            fprintf(stderr, "malloc failed in thread %lu at iteration %d\n", pthread_self(),i);
+            pthread_exit(NULL);
+        }
+        free(ptr);
+    }
+    pthread_exit(NULL);
 }
 
 // Test the performance of my_malloc/my_free
@@ -39,6 +82,45 @@ void test_my_allocator_performance(int num_allocations,size_t min_allocation_siz
     printf("my_malloc/my_free: %d allocations of sizes between %zu and %zu bytes took %f seconds\n",num_allocations, min_allocation_size, max_allocation_size, time_spent);
 }
 
+//Multithreaded performance testing of my_malloc/my_free
+void test_multithread_allocator_performance(int num_allocations,int num_threads,size_t min_allocation_size, size_t max_allocation_size)
+{
+    pthread_t threads[num_threads];
+    thread_data_t datas[num_threads];
+
+    //Set up test data for each thread
+    for (int i=0;i<num_threads;i++)
+    {
+        datas[i].num_allocations=num_allocations/num_threads;
+        datas[i].min_allocation_size=min_allocation_size;
+        datas[i].max_allocation_size=max_allocation_size;
+    }
+
+    struct timespec start,end;
+    //Start timing
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    //Creat threads
+    for (int i=0;i<num_threads;i++)
+    {
+        pthread_create(&threads[i],NULL,thread_task_custom,(void *)&datas[i]);
+    }
+
+    //Wait for all threads to complete
+    for (int i=0;i<num_threads;i++)
+    {
+        pthread_join(threads[i],NULL);
+    }
+
+    // End timing
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double time_spent = calculate_time(start, end);
+
+    printf("Custom Allocator (my_malloc/my_free): %d threads, %d allocations, "
+           "sizes between %zu and %zu bytes took %f seconds\n",
+           num_threads,num_allocations,min_allocation_size,max_allocation_size,time_spent);
+}
+
 // Test the performance of malloc/free
 void test_system_allocator_performance(int num_allocations,size_t min_allocation_size, size_t max_allocation_size) {
     struct timespec start, end;
@@ -62,4 +144,43 @@ void test_system_allocator_performance(int num_allocations,size_t min_allocation
 
     printf("malloc/free: %d allocations of sizes between %zu and %zu bytes took %f seconds\n",
            num_allocations, min_allocation_size, max_allocation_size, time_spent);
+}
+
+//Multithreaded performance testing of malloc/free
+void test_multithread_system_allocator_performance(int num_allocations,int num_threads,size_t min_allocation_size, size_t max_allocation_size)
+{
+    pthread_t threads[num_threads];
+    thread_data_t datas[num_threads];
+
+    //Set up test data for each thread
+    for (int i=0;i<num_threads;i++)
+    {
+        datas[i].num_allocations=num_allocations/num_threads;
+        datas[i].min_allocation_size=min_allocation_size;
+        datas[i].max_allocation_size=max_allocation_size;
+    }
+
+    struct timespec start,end;
+    //Start timing
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    //Creat threads
+    for (int i=0;i<num_threads;i++)
+    {
+        pthread_create(&threads[i],NULL,thread_task_system,(void *)&datas[i]);
+    }
+
+    //Wait for all threads to complete
+    for (int i=0;i<num_threads;i++)
+    {
+        pthread_join(threads[i],NULL);
+    }
+
+    // End timing
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    double time_spent = calculate_time(start, end);
+
+    printf("Custom Allocator (my_malloc/my_free): %d threads, %d allocations, "
+           "sizes between %zu and %zu bytes took %f seconds\n",
+           num_threads,num_allocations,min_allocation_size,max_allocation_size,time_spent);
 }
